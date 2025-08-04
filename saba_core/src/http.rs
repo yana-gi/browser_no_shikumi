1ae3f82eb43274c2f70fedd13d9b9cd114554c21
yana-gi <yanagisawa.hika@gmail.com>
2025-08-04 23:26:10 +0900

    テスト追加

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -1,10 +1,10 @@
 use alloc::string::String;
 use alloc::vec::Vec;
-use create::error::Error;
+use crate::error::Error;
 use alloc::format;
-use create::alloc::string::ToString;
+use alloc::string::ToString;
 
-#[derive(Debug, Cline)]
+#[derive(Debug, Clone)]
 pub struct HttpResponse {
     version: String,
     status_code: u32,
@@ -13,6 +13,7 @@ pub struct HttpResponse {
     body: String,
 }
 
+#[derive(Debug, Clone)]
 pub struct Header {
     name: String,
     value: String,
@@ -31,7 +32,7 @@ impl HttpResponse {
         let preprocessed_response = raw_response.trim_start().replace("\r\n", "\n");
 
         // ステータスラインの分割
-        let (status_line,remaining) = match preprocessed_response.spilit_once('\n') {
+        let (status_line,remaining) = match preprocessed_response.split_once('\n') {
             Some((s, r)) => (s, r),
             None => return Err(Error::UnexpectedResponse(format!("Invalid HTTP response: {}", raw_response))),
         };
@@ -41,22 +42,24 @@ impl HttpResponse {
             Some((h,b)) => {
                 let mut headers = Vec::new();
                 for header in h.split("\n") {
-                    let spilitted_header: Vec<&str> = header.splitn(2, ':').collect();
-                    headers.push(Header::new(String::from(spilitted_header[0]), String::from(spilitted_header[1]).trim()));
+                    let split_header: Vec<&str> = header.splitn(2, ':').collect();
+                    if split_header.len() == 2 {
+                        headers.push(Header::new(String::from(split_header[0]), String::from(split_header[1]).trim().to_string()));
+                    }
                 }
                 (headers,b)
             }
             None => (Vec::new(), remaining),
         };
 
-        let statuses: Vec<&str> = status_line.spilit(' ').collect();
+        let statuses: Vec<&str> = status_line.split(' ').collect();
 
         Ok(Self {
             version: statuses[0].to_string(),
             status_code: statuses[1].parse().unwrap_or(404),
             reason: statuses[2].to_string(),
             headers,
-            body,
+            body: body.trim().to_string(),
         })
     }
 
@@ -91,3 +94,59 @@ impl HttpResponse {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_status_line_only() {
+        let raw = "HTTP/1.1 200 OK\n".to_string();
+        let res = HttpResponse::new(raw).expect("Failed to parse http response");
+        assert_eq!(res.version(), "HTTP/1.1");
+        assert_eq!(res.status_code(), 200);
+        assert_eq!(res.reason(), "OK");
+        assert_eq!(res.headers().len(), 0);
+    }
+
+    #[test]
+    fn test_one_header() {
+        let raw = "HTTP/1.1 200 OK\nDate: Mon, 04 Aug 2025 00:00:00 GMT\n\n".to_string();
+        let res = HttpResponse::new(raw).expect("Failed to parse http response");
+        assert_eq!(res.version(), "HTTP/1.1");
+        assert_eq!(res.status_code(), 200);
+        assert_eq!(res.reason(), "OK");
+        assert_eq!(res.headers().len(), 1);
+
+        assert_eq!(res.header_value("Date").unwrap(), "Mon, 04 Aug 2025 00:00:00 GMT");
+    }
+
+    #[test]
+    fn test_two_headers_with_white_space() {
+        let raw = "HTTP/1.1 200 OK\nDate: Mon, 04 Aug 2025 00:00:00 GMT\nContent-length:42\n\n".to_string();
+        let res = HttpResponse::new(raw).expect("Failed to parse http response");
+        assert_eq!(res.version(), "HTTP/1.1");
+        assert_eq!(res.status_code(), 200);
+        assert_eq!(res.reason(), "OK");
+        assert_eq!(res.headers().len(), 2);
+        assert_eq!(res.header_value("Date").unwrap(), "Mon, 04 Aug 2025 00:00:00 GMT");
+        assert_eq!(res.header_value("Content-length").unwrap(), "42".to_string());
+    }
+
+    #[test]
+    fn test_body() {
+        let raw = "HTTP/1.1 200 OK\n\nHello, World!".to_string();
+        let res = HttpResponse::new(raw).expect("Failed to parse http response");
+        assert_eq!(res.version(), "HTTP/1.1");
+        assert_eq!(res.status_code(), 200);
+        assert_eq!(res.reason(), "OK");
+        assert_eq!(res.headers().len(), 0);
+        assert_eq!(res.body(), "Hello, World!".to_string());
+    }
+
+    #[test]
+    fn test_invalid() {
+        let raw = "Invalid HTTP response".to_string();
+
+        assert!(HttpResponse::new(raw).is_err());
+    }
+}
