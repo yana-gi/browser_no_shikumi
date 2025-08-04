f5787f834a46286b2ab490b96cf0d010a88e33e2
yana-gi <yanagisawa.hika@gmail.com>
2025-08-05 00:01:36 +0900

    Wasabi上で動くようになった

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,14 +1,13 @@
 extern crate alloc;
-use alloc::string::String;
-use saba_core::error::Error;
-use saba_core::http::HttpResponse;
 use alloc::format;
+use alloc::string::String;
 use alloc::string::ToString;
+use alloc::vec::Vec;
 use noli::net::lookup_host;
 use noli::net::SocketAddr;
 use noli::net::TcpStream;
-use alloc::vec::Vec;
-
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
 
 pub struct HttpClient {}
 
@@ -16,38 +15,52 @@ impl HttpClient {
     pub fn new() -> Self {
         Self {}
     }
-    pub fn get(&self, host:String, port:u16,path:String) -> Result<HttpResponse, Error> {
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
         let ips = match lookup_host(&host) {
             Ok(ips) => ips,
-            Err(e) => return Err(Error::Network(format!("Failed to lookup find IP address: {:#?}", e))),
+            Err(e) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP addresses: {:#?}",
+                    e
+                )))
+            }
         };
 
-        if ips.len() < 1{
-            return Err(Error::Network(format!("Failed to find IP address for host: {:?}", host)));
+        if ips.len() < 1 {
+            return Err(Error::Network("Failed to find IP addresses".to_string()));
         }
 
         let socket_addr: SocketAddr = (ips[0], port).into();
+
         let mut stream = match TcpStream::connect(socket_addr) {
             Ok(stream) => stream,
-            Err(_) => return Err(Error::Network("Failed to connect to TCP stream".to_string())),
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to connect to TCP stream".to_string(),
+                ))
+            }
         };
 
-        let mut request = String::from("GET ");
+        let mut request = String::from("GET /");
         request.push_str(&path);
-        request.push_str(" HTTP/1.1\r\n");
+        request.push_str(" HTTP/1.1\n");
 
         // ヘッダーを追加
         request.push_str("Host: ");
         request.push_str(&host);
-        request.push_str("\n");
+        request.push('\n');
         request.push_str("Accept: text/html\n");
-        request.push_str("\n");
-        
+        request.push_str("Connection: close\n");
+        request.push('\n');
+
         // リクエストを送信
         let _bytes_written = match stream.write(request.as_bytes()) {
             Ok(bytes) => bytes,
             Err(_) => {
-                return Err(Error::Network("Failed to write request to TCP stream".to_string()));
+                return Err(Error::Network(
+                    "Failed to send a request to TCP stream".to_string(),
+                ))
             }
         };
         // レスポンスの受信
@@ -58,7 +71,9 @@ impl HttpClient {
             let bytes_read = match stream.read(&mut buf) {
                 Ok(bytes) => bytes,
                 Err(_) => {
-                    return Err(Error::Network("Failed to read response from TCP stream".to_string()));
+                    return Err(Error::Network(
+                        "Failed to receive a request from TCP stream".to_string(),
+                    ))
                 }
             };
             // ストリームから読み込むバイトがなくなるまでroopを回す
@@ -67,9 +82,10 @@ impl HttpClient {
             }
             received.extend_from_slice(&buf[..bytes_read]);
         }
+
         match core::str::from_utf8(&received) {
             Ok(response) => HttpResponse::new(response.to_string()),
-            Err(e) => return Err(Error::Network(format!("Failed to parse response from TCP stream: {:#?}", e))),
+            Err(e) => Err(Error::Network(format!("Invalid received response: {}", e))),
         }
     }
 }
